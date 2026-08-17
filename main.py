1690bb03c801130195749a1bfa1d34f799da1129
Seyladavid@Git <chanseylasambath@gmail.com>
2026-08-17 14:09:09 -0400

    Initial commit first file

---
+++ main.py
@@ -0,0 +1,35 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('David Sambath; welcome to Python World')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+x_1 = 19
+y = int(3.25)
+z = "Hello"
+print(type(x_1))
+print(type(y))
+print(type(z))
+
+print(random.randrange(y, x_1))
+
+for x in "Seyladavid":
+  print(str(x))
+
+txt = "The best things in life is knowledge and freedown"
+if "life" not in txt:
+  print(txt[-22:-13])
+else:
+  print(txt[4:])
